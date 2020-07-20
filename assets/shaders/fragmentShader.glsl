f51aa0d0f3ecea78ddf20630f615ae7818e3d7f6
Your Name <dev.dead.tech@gmail.com>
2020-07-20 14:50:26 +0200

    Added a startNode bool, that helps me understand whether i should set the Frag_Color output variable to be either red or white.

--- assets/shaders/fragmentShader.glsl
+++ assets/shaders/fragmentShader.glsl
@@ -1,6 +1,11 @@
 #version 450 core
 out vec4 FragColor;
+uniform bool startNode;
 void main()
 {
-    FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    if(startNode){
+        FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    } else {
+        FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    }
 };
