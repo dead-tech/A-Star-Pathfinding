3890ec969252b9a0b69eeae6dd2b3940d3bb7570
Your Name <dev.dead.tech@gmail.com>
2020-07-20 09:54:33 +0200

    Added Model View Projection (M.V.P.) Matrices Multiplication

--- assets/shaders/vertexShader.glsl
+++ assets/shaders/vertexShader.glsl
@@ -1,10 +1,11 @@
 #version 450 core
 layout(location = 0) in vec3 position;
 
-uniform mat4 scaleTransform;
-uniform mat4 projection;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
 
 void main()
 {
-    gl_Position = projection * scaleTransform * vec4(position, 1.0f);
+    gl_Position = proj * view * model * vec4(position, 1.0f);
 }
