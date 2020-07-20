b9073cc7c378ac7a7b67a49a2ef0888bd6e99ac0
Your Name <dev.dead.tech@gmail.com>
2020-07-20 15:48:40 +0200

    Added a uniform for coloring the endNode

--- assets/shaders/fragmentShader.glsl
+++ assets/shaders/fragmentShader.glsl
@@ -1,10 +1,13 @@
 #version 450 core
 out vec4 FragColor;
 uniform bool startNode;
+uniform bool endNode;
 void main()
 {
     if(startNode){
         FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    } else if (endNode) {
+        FragColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);
     } else {
         FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
     }
